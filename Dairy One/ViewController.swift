37438372224e8e6ea2721eebc8c19770853b3210
zhenghy <zhenghy723@126.com>
2017-11-03 00:01:04 -0400

    Finish the feature of camera

--- Dairy One/ViewController.swift
+++ Dairy One/ViewController.swift
@@ -7,13 +7,63 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,AVCaptureVideoDataOutputSampleBufferDelegate {
+	let captureSession = AVCaptureSession();
+	var previewLayer:CALayer!;
+	
+	var captureDevice:AVCaptureDevice!;
+	var takephoto = false;
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		prepareCamera();
+		
+		
 	}
+	
+	func prepareCamera(){
+		captureSession.sessionPreset = AVCaptureSession.Preset.photo
+		
+		let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes:[.builtInWideAngleCamera],mediaType:AVMediaType.video,position:.front).devices
+		
+		captureDevice = availableDevices.first;
+		beginSession()
+	}
+	
+	func beginSession() {
+		do {
+			let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+			captureSession.addInput(captureDeviceInput)
+			
+		}catch{
+			print(error.localizedDescription)
+		}
+		let preview = AVCaptureVideoPreviewLayer(session: captureSession)
+		self.previewLayer = preview;
+		self.view.layer.addSublayer(self.previewLayer)
+		self.previewLayer.frame = self.view.layer.frame
+		captureSession.startRunning()
+		
+		let dataOutput = AVCaptureVideoDataOutput()
+		dataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as String) : NSNumber(value:kCVPixelFormatType_32BGRA)]
+		if captureSession.canAddOutput(dataOutput) {
+			captureSession.addOutput(dataOutput)
+			
+		}
+		captureSession.commitConfiguration()
+		
+		let queue = DispatchQueue(label:"haoyuan")
+		dataOutput.setSampleBufferDelegate(self, queue: queue)
+	
+	}
+	
+	func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+		<#code#>
+	}
+	
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
